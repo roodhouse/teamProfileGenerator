5d678f31e3ce36ec8fd78fa84a777dd6c0be1dc8
roodhouse <rughjm@gmail.com>
2022-10-09 15:53:26 -0500

    have html file being written through prompt

--- index.js
+++ index.js
@@ -3,51 +3,25 @@ const {writeFile}        = require('fs').promises;
 const generateHtml       = require('./src/generateHtml') 
 console.log(generateHtml)
 
-const firstQuestionsPrompt = {
-    type: 'input',
-    name: 'name',
-    message: 'Please provide your name.'
-  };
+// const firstQuestionsPrompt = {
+//     type: 'input',
+//     name: 'name',
+//     message: 'Please provide your name.'
+//   };
 
-  function main() {
-    console.log('Welcome manager!');
-    sayMyName();
-  }
-
-  main();
-
-  function sayMyName() {
-    inquirer.prompt(firstQuestionsPrompt).then((answers) => {
-        if(answers.firstQuestionsPrompt === 'John') {
-            console.log('hi');
-        } else {
-            console.log('bye')
-        }
-    }
-  )}
-
-//   function exitHouse() {
-//     inquirer.prompt(directionsPrompt).then((answers) => {
-//       if (answers.direction === 'Forward') {
-//         console.log('You find yourself in a forest');
-//         console.log(
-//           'There is a wolf in front of you; a friendly looking dwarf to the right and an impasse to the left.'
-//         );
-//         encounter1();
-//       } else {
-//         console.log('You cannot go that way. Try again');
-//         exitHouse();
-//       }
-//     });
+//   function main() {
+//     console.log('Welcome manager!');
+//     sayMyName();
 //   }
-  
 
-// const firstQuestions = inquirer.prompt([
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: 'Welcome manager. Please provide your name.',
-//     },
+//   main();
+
+const firstQuestions = inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Welcome manager. Please provide your name.',
+    },
 //     {
 //         type: 'input',
 //         name: 'id',
@@ -69,11 +43,35 @@ const firstQuestionsPrompt = {
 //         message: 'Which type of team member would you like to add?',
 //         choices: ['Engineer', 'Intern', 'I am finished adding team members'],
 //     }
-// ])
+]).then((answers) => writeFile('index2.html', generateHtml(answers))
+  .then(() => console.log('File was created'))
+  .then(() => console.log(answers))
+  .catch((err) => console.log(err))
+);
 
+//   function sayMyName() {
+//     inquirer.prompt(firstQuestionsPrompt).then((answers) => {
+//         if(answers.firstQuestionsPrompt === 'John') {
+//             console.log('hi');
+//         } else {
+//             console.log('bye')
+//         }
+//     }
+//   )}
 
-// .then((answers) => writeFile('README.md', generateHtml(answers))
-//   .then(() => console.log('File was created'))
-//   .then(() => console.log(answers))
-//   .catch((err) => console.log(err))
-// );
+//   function exitHouse() {
+//     inquirer.prompt(directionsPrompt).then((answers) => {
+//       if (answers.direction === 'Forward') {
+//         console.log('You find yourself in a forest');
+//         console.log(
+//           'There is a wolf in front of you; a friendly looking dwarf to the right and an impasse to the left.'
+//         );
+//         encounter1();
+//       } else {
+//         console.log('You cannot go that way. Try again');
+//         exitHouse();
+//       }
+//     });
+//   }
+  
+
