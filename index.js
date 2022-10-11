bb9d4e649ebd51cea5d9e50e269dd4c87c6cf930
roodhouse <rughjm@gmail.com>
2022-10-11 11:37:06 -0500

    failed attempts at saving more than one to the file

--- index.js
+++ index.js
@@ -2,40 +2,6 @@ const inquirer           = require('inquirer');
 const {writeFile}        = require('fs').promises;
 const {appendFile}        = require('fs').promises;
 const generateHtml       = require('./src/generateHtml') 
-console.log(generateHtml)
-
-// const firstQuestions = inquirer.prompt([
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: 'Welcome manager. Please provide your name.',
-//     },
-//     {
-//         type: 'input',
-//         name: 'id',
-//         message: 'Please verify your employee ID.',
-//     },
-//     {
-//         type: 'input',
-//         name: 'email',
-//         message: 'What is your email address?',
-//     },
-//     {
-//         type: 'input',
-//         name: 'office',
-//         message: 'Please provide your office number.',
-//     },
-//     {
-//         type: 'list',
-//         name: 'team',
-//         message: 'Which type of team member would you like to add?',
-//         choices: ['Engineer', 'Intern', 'I am finished adding team members'],
-//     },
-// ]).then((answers) => writeFile('index2.html', generateHtml(answers))
-//   .then(() => console.log('File was created'))
-//   .then(() => console.log(answers))
-//   .catch((err) => console.log(err))
-// )
 
 const rolePrompt = {
   type: 'list',
@@ -61,7 +27,7 @@ function roleSelection() {
       console.log('fail')
     }
   });
-}
+
 
   function managerQuestions() {
     console.log('Manager!');
@@ -167,5 +133,5 @@ function internQuestions() {
   writeFile('index2.html', generateHtml(answers))
   })
 }
-
+}
 main();
